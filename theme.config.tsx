c422fb5d74f326f1d9285f1c5a788d8a70d98e8f
demola234 <kolawoleoluwasegun567@gmail.com>
2025-03-24 16:36:52 +0100

    docs: documentation improvements

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
+
 
 const config: DocsThemeConfig = {
-  logo: <span>Flutter Bunny CLI</span>,
+  logo: (
+    <div className="flex items-center">
+      <Image
+        src="/bunny_logo.png"
+        alt="logo"
+        width={40}
+        height={40}
+      />
+    </div>
+  ),
   project: {
     link: 'https://github.com/demola234/flutter_bunny_cli/',
   },
-    docsRepositoryBase: 'https://github.com/demola234/flutter_bunny_cli/docs',
+  docsRepositoryBase: 'https://github.com/demola234/flutter_bunny_cli/docs',
   footer: {
     text: 'Flutter Bunny CLI Docs',
   },

70cd4ee38987367358cf7f13823fefd6075266f6
demola234 <kolawoleoluwasegun567@gmail.com>
2025-03-24 16:40:09 +0100

    docs: documentation favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -21,6 +21,21 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Flutter Bunny CLI Docs',
   },
+  head: (
+    <>
+      <link rel="icon" type="image/png" href="/bunny_logo.png" />
+    </>
+  ),
+//   head: (
+//       <>
+//         <link rel="icon" href="/favicon.ico" sizes="any" />
+//         <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+//         <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+//         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//         <meta name="description" content="Flutter Bunny CLI Documentation" />
+//       </>
+//     ),
 }
 
 export default config

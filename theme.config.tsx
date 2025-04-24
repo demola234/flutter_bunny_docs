1508015d9a255d2685cd9ad0162f3f5057130e2c
demola234 <kolawoleoluwasegun567@gmail.com>
2025-04-24 05:09:51 +0100

    docs: remove Set Up Guide and add Firebase and Design System guides to documentation

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
 import Image from 'next/image'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: (
